1b64411ec59d007faffe6ff2608bacfd4c5b5353
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-05-12 16:55:12 +0100

    first version of lazyObject

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+
+struct lazyObject {
+    //points to code to be executed when the object is accessed
+    void (*code)();
+    // points to the data of the object
+    void *data;
+    // flag to check if the code has been executed (0 for not executed, 1 for executed)
+    int executed; 
+} lazyObject;
+
 int main() {
+    // initialize the lazy object
+    lazyObject.code = NULL; 
+    lazyObject.data = NULL;
+    lazyObject.executed = 0;
     printf("Hello, World!\n");
     return 0;
 }
